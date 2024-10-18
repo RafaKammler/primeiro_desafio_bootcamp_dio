fc176016a3f7de78f708c38b1fef0b685ad5fbdc
RafaKammler <rafaelgkammler@gmail.com>
2024-10-18 15:29:21 -0300

    adicionando resolução do desafio

--- Exercicio.js
+++ Exercicio.js
@@ -1,32 +1,23 @@
-let nome_heroi = "Jogador 1"
-let XP = 6000
-let rank = ""
+let nome_heroi = "Jogador 1";
+let XP = 6000;
+let rank = "";
 
-switch (XP){
-case XP < 1000:
-        rank = "Ferro"
-        break;
-case XP >= 1001 && XP <= 2000:
-        rank = "Bronze"
-        break;
-case XP >= 2001 && XP <= 5000:
-        rank = "Prata"
-        break;
-case XP >= 5001 && XP <= 7000:
-        rank = "Ouro"
-        break;
-case XP >= 7001 && XP <= 8000:
-        rank = "Platina"
-        break;
-case XP >= 8001 && XP <= 9000:
-        rank = "Ascendente"
-        break;
-case XP >= 9001 && XP <= 10000:
-        rank = "Imortal"
-        break;
-case XP >= 10001:
-        rank = "Radiante"
-        break;
+if (XP < 1000) {
+        rank = "Ferro";
+} else if (XP >= 1001 && XP <= 2000) {
+        rank = "Bronze";
+} else if (XP >= 2001 && XP <= 5000) {
+        rank = "Prata";
+} else if (XP >= 5001 && XP <= 7000) {
+        rank = "Ouro";
+} else if (XP >= 7001 && XP <= 8000) {
+        rank = "Platina";
+} else if (XP >= 8001 && XP <= 9000) {
+        rank = "Ascendente";
+} else if (XP >= 9001 && XP <= 10000) {
+        rank = "Imortal";
+} else if (XP >= 10001) {
+        rank = "Radiante";
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${rank}`)
+console.log(`O Herói de nome ${nome_heroi} está no nível de ${rank}`);
